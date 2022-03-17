d74135b1b74587149bfad6f1880655792e8c0a76
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2022-03-16 19:56:19 -0600

    addTask function is created and passed down to ToDoForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
+import ToDoForm from './components/ToDoForm';
+import data from './data.json'
+import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState(data);
+
+  const addTask = (userInput) => {
+    setTodoList([
+      ...todoList,
+      {
+        id: todoList.length + 1,
+        task: userInput,
+        complete: false
+      }
+    ]);
+  };
+
   return (
     <div className="todo-app">
       <Header />
+      <ToDoForm addTask={addTask} />
     </div>
   );
 }
